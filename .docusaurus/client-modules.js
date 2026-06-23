451d7f447f997397d0a0f8759660a8ff926b12f8
Marie-Angélique Laporte <marieangelique.laporte@gmail.com>
2026-06-23 09:15:28 +0200

    Stop tracking generated and dependency files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/malaporte/Documents/GitHub/documentation/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/malaporte/Documents/GitHub/documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/malaporte/Documents/GitHub/documentation/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/malaporte/Documents/GitHub/documentation/src/css/custom.css"),
-];
